0735a0d9239ea1128759aa984babfe4b0ff88dc3
BIRSAN RADU IOSIF <radubirsan>
2026-05-13 14:47:56 +0300

    initial commit

---
+++ Stocky/App/StockyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockyApp.swift
+//  Stocky
+//
+//  Created by radu on 13.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct StockyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
